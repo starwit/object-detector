a8189f6fc082c76122d591ecc1b3c475fa9d2627
flonix8 <flstanek@googlemail.com>
2023-08-17 15:26:48 +0200

    Change redis keys naming scheme (use ':' as namespace separator)

--- run_stage.py
+++ run_stage.py
@@ -5,7 +5,7 @@ from objectdetector.config import ObjectDetectorConfig
 from objectdetector.detector import Detector
 
 def extract_stream_id(input_stream_name: str) -> str:
-    return input_stream_name.removeprefix("video_source_")
+    return input_stream_name.split(':')[1]
 
 if __name__ == '__main__':
 
@@ -35,13 +35,13 @@ if __name__ == '__main__':
 
     # Start processing images
     while not stop_event.is_set():
-        
+
         input_okay = False
         while not input_okay and not stop_event.is_set():
             result = redis_conn.xread(
                 count=1,
                 block=5000,
-                streams={f'video_source_{id}': '$' if last_retrieved_id is None else last_retrieved_id 
+                streams={f'videosource:{id}': '$' if last_retrieved_id is None else last_retrieved_id 
                          for id in CONFIG.redis.video_source_ids}
             )
         
@@ -58,4 +58,4 @@ if __name__ == '__main__':
         output_proto = detector.get(input_proto)
 
         if output_proto is not None:
-            redis_conn.xadd(name=f'object_detector_{extract_stream_id(input_stream)}', fields={'proto_data': output_proto}, maxlen=10)
+            redis_conn.xadd(name=f'objectdetector:{extract_stream_id(input_stream)}', fields={'proto_data': output_proto}, maxlen=10)
