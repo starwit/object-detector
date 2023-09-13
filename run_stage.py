074528f3dac2c316bbf7cb3c8d43624543e52007
flonix8 <flstanek@googlemail.com>
2023-09-13 15:57:29 +0200

    Version 0.5.0: Incorporate common components from visionlib

--- run_stage.py
+++ run_stage.py
@@ -3,6 +3,8 @@ import threading
 from typing import List
 
 import redis
+from visionlib.pipeline.consumer import RedisConsumer
+from visionlib.pipeline.publisher import RedisPublisher
 
 from objectdetector.config import ObjectDetectorConfig
 from objectdetector.detector import Detector
@@ -10,35 +12,6 @@ from objectdetector.detector import Detector
 
 def extract_stream_id(input_stream_name: str) -> str:
     return input_stream_name.split(':')[1]
-
-
-class RedisListener:
-
-    def __init__(self, redis_client: redis.Redis, stream_ids: List[str]) -> None:
-        self._redis_client = redis_client
-        self._stream_ids = stream_ids
-
-        self._last_retrieved_id: str = None
-        self._current_stream_idx: int = 0
-
-    def get_new_message(self):
-        result = redis_conn.xread(
-            count=1,
-            block=2000,
-            streams={f'videosource:{id}': '$' if self._last_retrieved_id is None else self._last_retrieved_id 
-                        for id in CONFIG.redis.stream_ids}
-        )
-        
-        if result is None or len(result) == 0:
-            return None
-        
-        self._last_retrieved_id = result[0][1][0][0].decode('utf-8')
-
-        return result
-
-    def _incr_stream_idx(self):
-        self._current_stream_idx = (self._current_stream_idx + 1) % len(self._stream_ids)
-
 
 if __name__ == '__main__':
 
@@ -57,33 +30,22 @@ if __name__ == '__main__':
     CONFIG = ObjectDetectorConfig()
 
     detector = Detector(CONFIG)
+
+    publish = RedisPublisher(CONFIG.redis.host, CONFIG.redis.port)
+    consume = RedisConsumer(CONFIG.redis.host, CONFIG.redis.port, stream_keys=CONFIG.redis.stream_ids)
     
-    redis_conn = redis.Redis(
-        host=CONFIG.redis.host,
-        port=CONFIG.redis.port,
-    )
-    
-    redis_listener = RedisListener(redis_conn, CONFIG.redis.stream_ids)
+    with publish, consume:
+        for stream_key, proto_data in consume():
+            if stream_key is None:
+                continue
 
-    # Start processing images
-    while not stop_event.is_set():
+            output_proto_data = detector.get(proto_data)
 
-        reply = redis_listener.get_new_message()
+            if output_proto_data is None:
+                continue
 
-        if reply is None:
-            continue
+            publish(stream_key, proto_data)
 
-        for item in reply:
             if stop_event.is_set():
                 break
             
-            # These unpacking incantations are ugly but necessary due to redis reply structure
-            input_proto = item[1][0][1][b'proto_data']
-            input_stream = item[0].decode('utf-8')
-
-            output_proto = detector.get(input_proto)
-
-            if output_proto is None:
-                continue
-
-            redis_conn.xadd(name=f'objectdetector:{extract_stream_id(input_stream)}', fields={'proto_data': output_proto}, maxlen=10)
