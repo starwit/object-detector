de80ed334738515a9f8cbf97db3e40b11e66da20
flonix8 <flstanek@googlemail.com>
2025-06-17 11:37:06 +0200

    Remove unnecessary variable

--- objectdetector/stage.py
+++ objectdetector/stage.py
@@ -12,8 +12,6 @@ from .detector import Detector
 
 logger = logging.getLogger(__name__)
 
-PROMETHEUS_METRICS_PORT = 8000
-
 REDIS_PUBLISH_DURATION = Histogram('object_detector_redis_publish_duration', 'The time it takes to push a message onto the Redis stream',
                                    buckets=(0.0025, 0.005, 0.0075, 0.01, 0.025, 0.05, 0.075, 0.1, 0.15, 0.2, 0.25))
 FRAME_COUNTER = Counter('object_detector_frame_counter', 'How many frames have been consumed from the Redis input stream')
